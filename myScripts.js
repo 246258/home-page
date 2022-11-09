c627e4154dd2bd4c8601312613d7403d5698afbb
246258 <91135136+246258@users.noreply.github.com>
2022-11-09 13:21:11 -0600

    11/9 commit

--- myScripts.js
+++ myScripts.js
@@ -1,9 +1,9 @@
 //CLOCK SCRIPT
 function startTime() {
-    const today = new Date(1.33);
-    let h = today.getHours(12);
-    let m = today.getMinutes(59);
-    let s = today.getSeconds(60);
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
@@ -14,4 +14,44 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
+
+//POST-IT NOTE (JAVASCRIPT)
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+}
    
+   function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+}
+
+var show = false;
+function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+
+ //TWITTER
+ var showTweet = false;
+ function showTwitter(){
+     if (showTweet == false){
+         document.getElementById("tweet").style.opacity = 1;
+         document.getElementById("tweet").style.zIndex = 1;
+         showTweet = true;
+     }
+  }
+ 
+  function hideTweet(){
+     if (showTweet == true){
+         document.getElementById("tweet").style.opacity = 0;
+         document.getElementById("tweet").style.zIndex = -1;
+         showTweet = false;
+     }
+  }
